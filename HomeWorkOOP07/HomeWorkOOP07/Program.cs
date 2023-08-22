378ed60de20b24de059ec17f085ebe70fe350861
ismoil8282 <137428819+ismoil8282@users.noreply.github.com>
2023-08-22 20:21:04 +0300

    1

--- HomeWorkOOP07/HomeWorkOOP07/Program.cs
+++ HomeWorkOOP07/HomeWorkOOP07/Program.cs
@@ -7,12 +7,14 @@
             Hero lion = new Lion("Lion", 25, 50, 100);
             lion.Display();
             lion.Run();
+
             Console.WriteLine();
+
             Hero murlock = new Murlock("Murlock", 20, 50, 80);
             murlock.Display();
             murlock.Run();
             murlock.Attack();
-            
+           
         }
     }
 }
